63e11c4d899f3e375101177492056afc47f2467e
Harshita <rathodha@msu.edu>
2024-03-24 16:47:19 -0400

    new

---
+++ app/src/main/java/msu/edu/rathodha/cse476project/MainActivity.java
@@ -0,0 +1,14 @@
+package msu.edu.rathodha.cse476project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
